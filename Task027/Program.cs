641d3eddf7817e946c9a56930668e91e38664781
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-09-05 13:09:36 +0300

    add Task 027 Natural Numbers M to N

---
+++ Task027/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        /* Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+    M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+    M = 4; N = 8. -> ""4, 6, 7, 8"" */
+
+        Console.Write("Введите число M: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+        int temp = 0;
+
+        if (m <= 0 || n <= 0)
+        {
+            Console.WriteLine("Введены некорректные данные");
+        }
+
+        if (m > n)
+        {
+            temp = m;
+            m = n;
+            n = temp;
+        }
+
+        for (int i = m; i <= n; i++)
+        {
+            Console.Write($"{i} ");
+        }
+
+    }
+}
