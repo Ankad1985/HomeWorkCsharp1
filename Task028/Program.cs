59195166eab2f34acd409812bff45f8f3d946b2f
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-09-05 13:13:27 +0300

    add Task 028 Sum Natural Numbers M to N

---
+++ Task028/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        /* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+       M = 1; N = 15 -> 120
+       M = 4; N = 8. -> 30 */
+        Console.Write("Введите число M: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+        int temp = 0;
+
+        if (m > n)
+        {
+            temp = m;
+            m = n;
+            n = temp;
+        }
+
+        //Рекурсия
+        int SumRec(int m, int n)
+        {
+            if (m == n) return n;
+            else return n + SumRec(m, n - 1);
+        }
+
+        Console.Write($"Суммма чисел в интервале от M до N: {SumRec(m, n)}");
+
+        /* Итеративный метод
+
+        int Sum(int m, int n)
+        {
+         int result = 0;
+         for (int i = m; i <= n; i++)
+         {
+           result += i;
+         } 
+         return result;
+        }
+        */
+
+
+    }
+}
